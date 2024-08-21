b75acff67e22d6f0f5c0e9a187605245ce06504c
Eastern-Dream <eyrie@posteo.net>
2024-08-21 18:05:53 -0400

    fixed scanning function

--- feature/printing.nix
+++ feature/printing.nix
@@ -15,9 +15,9 @@
 
   environment.systemPackages = with pkgs; [ libtiff_t ];
 
-  # environment.interactiveShellInit = ''
-  #   alias scan-start='export SCANPATH="~/Documents/printer-scan/$(date -Iseconds)" \
-  #   & scanimage --device-name="escl:https://192.168.1.4:443" --resolution 300 --format tiff \
-  #   > $SCANPATH.tiff & tiff2pdf -j -o $SCANPATH.pdf $SCANPATH.tiff'
-  # '';
+  environment.interactiveShellInit = ''
+    alias scan-start='TEMPFILE=$(mktemp) && \
+    scanimage --device-name="escl:https://192.168.1.4:443" --resolution 300 --format tiff > $TEMPFILE && \
+    tiff2pdf -j -o ~/Documents/printer-scan/$(date -Iseconds).pdf $TEMPFILE'
+  '';
 }
