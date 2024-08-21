9b584572af1a28d4044669483cbe3da5b452f7ba
Eastern-Dream <eyrie@posteo.net>
2024-08-21 18:06:01 -0400

    add lutris and prismlauncher

--- feature/gaming.nix
+++ feature/gaming.nix
@@ -1,7 +1,12 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
     # udev rules for tablets and controllers
     hardware.steam-hardware.enable = true;
     hardware.opentabletdriver.enable = true;
+
+    users.users.${config.identity.username}.packages = with pkgs; [
+        lutris
+        prismlauncher
+    ];
 }
