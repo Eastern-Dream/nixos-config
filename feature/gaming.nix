75ae316b5c81938848c61c6834ee18173fdf408c
Eastern-Dream <eyrie@posteo.net>
2024-09-21 15:42:24 -0400

    config file drop in steam

--- feature/gaming.nix
+++ feature/gaming.nix
@@ -13,11 +13,21 @@
     };
 
     # If native steam is used, then dont need these 2
-    hardware.opengl.driSupport32Bit = true;
-    hardware.steam-hardware.enable = true;
+    # hardware.opengl.driSupport32Bit = true;
+    # hardware.steam-hardware.enable = true;
 
     users.users.${config.identity.username}.packages = with pkgs; [
         lutris
         prismlauncher
     ];
+
+    # workaround for steam download speed issues on linux (https://github.com/ValveSoftware/steam-for-linux/issues/10248)
+    home-manager.users.${config.identity.username} = {
+        home.file.".var/app/com.valvesoftware.Steam/.local/share/Steam/steam_dev.cfg".text = ''
+            @nClientDownloadEnableHTTP2PlatformLinux 0
+            @fDownloadRateImprovementToAddAnotherConnection 1.1
+            @cMaxInitialDownloadSources 15
+            unShaderBackgroundProcessingThreads 8
+        '';
+    };
 }
