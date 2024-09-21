7d22aaaf614d996ddc9a1c5b2dcc045ad7f2318f
Eastern-Dream <eyrie@posteo.net>
2024-09-21 15:37:35 -0400

    init native steam

--- feature/gaming.nix
+++ feature/gaming.nix
@@ -2,8 +2,19 @@
 
 {
     # udev rules for tablets and controllers
-    hardware.steam-hardware.enable = true;
     hardware.opentabletdriver.enable = true;
+
+    # native steam stuff 
+    programs.gamemode.enable = true;
+    programs.gamescope.enable = true;
+    programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+    };
+
+    # If native steam is used, then dont need these 2
+    hardware.opengl.driSupport32Bit = true;
+    hardware.steam-hardware.enable = true;
 
     users.users.${config.identity.username}.packages = with pkgs; [
         lutris
