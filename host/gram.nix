9b42d278ba9f4d7186d71bd31da230bea6a2c0fb
Eastern-Dream <eyrie@posteo.net>
2024-10-09 14:53:51 -0400

    fix log spam on lg gram

--- host/gram.nix
+++ host/gram.nix
@@ -11,5 +11,10 @@
     };
 
     identity.hostname = "gram";
+    # Fix the log spam by disabling a driver
+    # https://www.reddit.com/r/linuxhardware/comments/x97m6l/comment/j2r7irr
+    boot.extraModprobeConfig = ''
+      blacklist int3403_thermal
+    '';
   };
 }
