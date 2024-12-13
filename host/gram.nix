ad43d2e2d635a81a9f1c8f621db228e6feff44e6
Eastern-Dream <eyrie@posteo.net>
2024-12-13 09:39:24 -0500

    laptop config

--- host/gram.nix
+++ host/gram.nix
@@ -16,6 +16,13 @@
     boot.extraModprobeConfig = ''
       blacklist int3403_thermal
     '';
-    boot.kernelParams = ["acpi_mask_gpe=0x6E"];
+    boot.kernelParams = [ 
+      # The line below likely to not do anything
+      #"acpi_mask_gpe=0x6E"
+      # The LG Gram does not support s3 sleep, using it will cause imminent restart after wakeup!
+      #"mem_sleep_default=deep"
+    ];
+    # Suspect the default suspend-then-hibernate has a funny complicated trigger so lets just force 1hr for now
+    systemd.sleep.extraConfig = "HibernateDelaySec=1h";
   };
 }
