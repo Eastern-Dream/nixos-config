4f7f1184965761bdffc2606264ec96aba9794ba4
Eastern-Dream <eyrie@posteo.net>
2024-12-13 09:39:08 -0500

    switch to lix for no reason at all

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,23 @@
       
       # System/host/identity/whatever you wanna call it
       ./host/default.nix
+
+            (let
+        module = fetchTarball {
+          name = "source";
+          url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
+          sha256 = "sha256-DN5/166jhiiAW0Uw6nueXaGTueVxhfZISAkoxasmz/g=";
+        };
+        lixSrc = fetchTarball {
+          name = "source";
+          url = "https://git.lix.systems/lix-project/lix/archive/2.91.1.tar.gz";
+          sha256 = "sha256-hiGtfzxFkDc9TSYsb96Whg0vnqBVV7CUxyscZNhed0U=";
+        };
+        # This is the core of the code you need; it is an exercise to the
+        # reader to write the sources in a nicer way, or by using npins or
+        # similar pinning tools.
+        in import "${module}/module.nix" { lix = lixSrc; }
+      )
     ];
   config = {
     # Zen kernel
